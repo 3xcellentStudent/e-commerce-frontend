13deb97a5078e47187b8f8c23643429cece66f19
3xcellentStudent <grelaxman2000@gmail.com>
2025-08-25 18:44:27 -0600

    Some commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-'use client'
+// 'use client'
 
-import {useEffect} from 'react'
-import {useRouter} from 'next/navigation'
+// import {useEffect} from 'react'
+// import {useRouter} from 'next/navigation'
 import Header from '@/components/common/Header/Header'
 
 export default function Home(){
 
-  const redirect = useRouter()
+  // const redirect = useRouter()
 
-  useEffect(() => {redirect.push('/product/d41d8cd9-8f00-3204-a980-0998ecf8427e')}, [])
+  // useEffect(() => {redirect.push('/product/d41d8cd9-8f00-3204-a980-0998ecf8427e')}, [])
 
   return(
     <body>
